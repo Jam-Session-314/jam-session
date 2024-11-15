ccbcb1c0a600f9256ec1b06d6495e1c4eaf99ff0
Lewen Lin <linlewen727@gmail.com>
2024-11-14 21:08:28 -1000

    create a branch to adjust global styling of page

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -16,17 +16,17 @@ const NavBar: React.FC = () => {
   return (
     <Navbar bg="light" expand="lg">
       <Container>
-        <Navbar.Brand href="/">Next.js Application Template</Navbar.Brand>
+        <Navbar.Brand href="/">JAM SESSION</Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="me-auto justify-content-start">
             {currentUser
               ? [
                   <Nav.Link id="add-stuff-nav" href="/add" key="add" active={pathName === '/add'}>
-                    Add Stuff
+                    SESSIONS
                   </Nav.Link>,
                   <Nav.Link id="list-stuff-nav" href="/list" key="list" active={pathName === '/list'}>
-                    List Stuff
+                    EXPLORE PROFILES
                   </Nav.Link>,
                 ]
               : ''}
