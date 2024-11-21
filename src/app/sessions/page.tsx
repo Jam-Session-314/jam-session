200f9f1beeb4346a9ac53f63575da557bce67825
codygt3 <codygt@hawaii.edu>
2024-11-21 11:48:39 -1000

    Added session view page, removed add and list pages from navbar

---
+++ src/app/sessions/page.tsx
@@ -0,0 +1,63 @@
+/* eslint-disable @typescript-eslint/no-shadow */
+import { getServerSession } from 'next-auth';
+import { Col, Container, Row } from 'react-bootstrap';
+import { loggedInProtectedPage } from '@/lib/page-protection';
+import { authOptions } from '@/app/api/auth/[...nextauth]/route';
+import SessionCard from '@/components/SessionCard';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+interface SessionType {
+  id: number;
+  location: string;
+  time: string;
+  musicalType: string;
+  desiredCapabilities: string;
+  organizerContact: string;
+}
+
+const SessionsPage = async () => {
+  const session = await getServerSession(authOptions);
+  loggedInProtectedPage(
+    session as {
+      user: { email: string; id: string; randomKey: string };
+    } | null,
+  );
+
+  // Fetch all sessions from the database
+  const sessions: SessionType[] = (await prisma.session.findMany({
+    select: {
+      id: true,
+      location: true,
+      time: true,
+      musicalType: true,
+      desiredCapabilities: true,
+      organizerContact: true,
+    },
+  })).map((session) => ({
+    ...session,
+    time: session.time.toISOString(), // Convert Date to string
+  }));
+
+  return (
+    <main>
+      <Container id="sessions" fluid className="py-3">
+        <Row>
+          <Col className="text-center">
+            <h1>Current Jam Sessions</h1>
+          </Col>
+        </Row>
+        <Row xs={1} md={2} lg={3} className="g-4">
+          {sessions.map((session) => (
+            <Col key={`Session-${session.id}`}>
+              <SessionCard session={session} />
+            </Col>
+          ))}
+        </Row>
+      </Container>
+    </main>
+  );
+};
+
+export default SessionsPage;
